8f25fdef40ae85d421e8f6f32e0f9fc68bc88fb1
Onehundra <hermanssonkevin@gmail.com>
2025-11-11 17:35:18 +0100

    hej

---
+++ PracticeDB1/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDB1
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PracticeDB1;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
+        }
+    }
+}
